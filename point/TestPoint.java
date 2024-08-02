af53a45fff9ed6303933a0ab7a3c19ba4f9aff01
Upendra Nilupul <upendranilupul2001@gmail.com>
2024-08-03 01:00:04 +0530

    Add files via upload

---
+++ point/TestPoint.java
@@ -0,0 +1,51 @@
+class TestPoint{
+	public static void main(String args[]){
+		Point p1 = new Point(4,5);
+		
+		
+		System.out.println(p1.distance(2,3));
+		System.out.println(p1.getX());
+	}
+}
+
+class Point{
+	private int x;
+	private int y;
+	
+	Point(){
+		
+	}
+	
+	Point(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public int getX(){
+		return x;
+	}
+	
+	public int getY(){
+		return y;
+	}
+	
+	public void setX(int x){
+		this.x = x;
+	}
+	
+	public void setY(int y){
+		this.y = y;
+	}
+	
+	public void setXY(int x, int y){
+		this.x = x;
+		this.y = y;
+	}
+	
+	public double distance(int x1, int y1){
+		int xDiff = this.x - x1;
+		int yDiff = this.y - y1;
+		
+		return (xDiff^2 + yDiff^2)^1/2;
+	}
+}
